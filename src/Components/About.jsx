fbbfe1e02826f3e8b9804e9cfbe1f5c454cf5eb3
Mk-Thakur <thakurmohit999131@gmail.com>
2024-09-27 15:41:31 +0530

    Initial commit

---
+++ src/Components/About.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { LiaSwatchbookSolid } from "react-icons/lia";
+import { IoGitNetworkOutline } from "react-icons/io5";
+import { BsDatabaseDown } from "react-icons/bs";
+import { IoShieldCheckmark } from "react-icons/io5";
+import "../Components/Global.css"
+
+const About = ({bgcolor,darkmode}) => {
+  return (
+    <>
+      <div className="relative h-full">
+        <div className={`${darkmode==="dark" ? `bg-${bgcolor}` : `bg-white`} p-10 rounded-lg`}>
+          <h1 className={`text-3xl font-bold ${bgcolor==="white" ? "text-black" :"text-white"}`} >About</h1>
+          <p className="my-4  lg:text-nowrap "
+           style={{ color: `${bgcolor==="white" ? "#000000" : "#A6A6A6"}`}}
+          >
+            I have Completed My <span className="font-bold">Diploma  </span>From Govt Polytechnic College  <br /> Rajpur Sadhaura Haryana
+            My Specilization in <span className="font-bold">Computer Science Engineering (CSE)</span>
+             
+            <br />{" "}
+          </p>
+
+          <p className="lg:text-nowrap"
+           style={{ color: `${bgcolor==="white" ? "#000000" : "#A6A6A6"}`}}       
+          >
+            My aim is to bring across your message and identity in the most
+            creative way. I <br />
+            creates <span className="font-bold">web design</span> for different screen sizes.<br/>
+            <br/>
+            Proven ability to create <span className="font-bold">reusable components </span> for an
+            enterprise-level web application.<br/>Passionate about <span className="font-bold">technology and building scalable web
+            Application </span>
+          </p>
+          <h1 className="text-2xl my-5 font-italic"
+           style={{ color: `${bgcolor==="white" ? "#000000" : "white"}`}}  
+          >What I DO!</h1>
+
+          <div className="flex gap-5 flex-wrap ">
+            <div className="ui-ux-design w-80 flex gap-3 p-4 rounded-lg" style={{background:`${bgcolor==="white" ? "#FFF4F4" :"black"}`,  border:`${bgcolor==="white" ? "none" :"1px solid #A6A6A6"}`}}>
+              <div>
+                <LiaSwatchbookSolid className="text-4xl my-2 text-purple-600" />
+              </div>
+              <div className="flex flex-col gap-1">
+                <h1 className={`font-bold my-2 ${bgcolor==="white" ? "text-black" :"text-white"}`} 
+                >Full stack web Developement</h1>
+                <p className="text-sm"
+                 style={{ color: `${bgcolor==="white" ? "#000000" : "#A6A6A6"}`}}
+                >
+                 With a deep understanding of <span className="font-bold">HTML, CSS, JavaScript,node.js,MongoDb</span>, and modern frameworks like <span className="font-bold">Bootstrap,React</span>
+                  .I transform design concepts into dynamic, user-friendly websites. 
+                </p>
+              </div>
+            </div>
+
+            <div className="ui-ux-design w-80 flex gap-3 p-4 rounded-lg" style={{background:`${bgcolor==="white" ? "#EEF5FA" : "black"}` ,border:`${bgcolor==="white" ? "none" :"1px solid #A6A6A6"}`}}>
+              <div>
+                <IoGitNetworkOutline className="text-4xl my-2 text-yellow-600" />
+              </div>
+              <div className="flex flex-col gap-1">
+                <h1 className={`font-bold my-2 ${bgcolor==="white" ? "text-black" :"text-white"}`}>Git & Github</h1>
+                <p className="text-sm"
+                  style={{ color: `${bgcolor==="white" ? "#000000" : "#A6A6A6"}`}}
+                >
+                   It's used for <span className="font-bold">storing, tracking, and collaborating on software projects </span>. It makes it easy for developers to share code files.
+                </p>
+              </div>
+            </div>
+
+            <div className="ui-ux-design w-80 flex gap-3 p-4 rounded-lg" style={{background:`${bgcolor==="white" ? "#EEF5FA" :"black"}`,border:`${bgcolor==="white" ? "none" :"1px solid #A6A6A6"}`}}>
+              <div>
+                <BsDatabaseDown className="text-4xl my-2 text-blue-400" />
+              </div>
+              
+            </div>
+
+            <div className="ui-ux-design w-80 flex gap-3 p-4 rounded-lg" style={{background:`${bgcolor==="white" ? "#FFF4F4" :"black"}`,border:`${bgcolor==="white" ? "none" :"1px solid #A6A6A6"}`}}>
+              <div>
+                <IoShieldCheckmark className="text-4xl my-2 text-green-500" />
+              </div>
+              
+            </div>
+          </div>
+        </div>
+
+        
+      </div>
+    </>
+  );
+};
+
+export default About;
